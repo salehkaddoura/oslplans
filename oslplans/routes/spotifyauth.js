0d5fa723b4d9f1dbebccbd8d65be347df2f75770
ahvigil <ahvigil@mail.sfsu.edu>
2014-07-27 00:39:04 -0700

    adjust spotify scopes

--- oslplans/routes/spotifyauth.js
+++ oslplans/routes/spotifyauth.js
@@ -33,7 +33,7 @@ router.get('/', function(req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email';
+  var scope = 'playlist-read-private user-library-read';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
