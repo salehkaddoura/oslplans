8703fabb3df3945c79f340d35bbc4eb4417f7ee3
ahvigil <ahvigil@mail.sfsu.edu>
2014-07-27 02:26:46 -0700

    debug

--- oslplans/routes/home.js
+++ oslplans/routes/home.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
+  access_token = req.query.access_token;
+  console.log(access_token);
   res.render('home', { title: 'Express' });
 });
 
