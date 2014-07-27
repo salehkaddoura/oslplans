cf2243b3223e932bc55bde9135b23b4b19952912
ahvigil <ahvigil@mail.sfsu.edu>
2014-07-27 02:28:25 -0700

    debug

--- oslplans/routes/home.js
+++ oslplans/routes/home.js
@@ -5,6 +5,18 @@ var router = express.Router();
 router.get('/', function(req, res) {
   access_token = req.query.access_token;
   console.log(access_token);
+  
+  var options = {
+    url: 'https://api.spotify.com/v1/me',
+    headers: { 'Authorization': 'Bearer ' + access_token },
+    json: true
+  };
+  
+  // use the access token to access the Spotify Web API
+  request.get(options, function(error, response, body) {
+    console.log(body);
+  });
+  
   res.render('home', { title: 'Express' });
 });
 
