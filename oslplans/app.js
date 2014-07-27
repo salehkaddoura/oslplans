fe88cea01ba4b013782b43d72dfdcc869f440380
Saleh Kaddoura <saleh.kaddoura@gmail.com>
2014-07-26 21:25:09 -0700

    adding semicolon

--- oslplans/app.js
+++ oslplans/app.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', login.home);
-app.use('/home', routes.index)
+app.use('/home', routes.index);
 app.use('/users', users);
 
 /// catch 404 and forward to error handler
