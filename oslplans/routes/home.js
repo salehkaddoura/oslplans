b1c4a5b3f7a45a3b5c08384317117e86282fbd6f
ahvigil <ahvigil@mail.sfsu.edu>
2014-07-27 01:55:12 -0700

    add home spotify api calls

--- oslplans/routes/home.js
+++ oslplans/routes/home.js
@@ -5,6 +5,20 @@ var router = express.Router();
 router.get('/', function(req, res) {
   access_token = req.query.code;
   console.log("Recieved access token: " + access_token);
+  
+  var options = {
+    url: 'https://api.spotify.com/v1/me',
+    headers: { 'Authorization': 'Bearer ' + access_token },
+    json: true
+  };
+  
+  request.get(options, function(error, response, body) {
+    user_id = body.id
+    console.log(user_id);
+  });
+  
+  
+  
   res.render('home', { title: 'Express' });
 });
 
