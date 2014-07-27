ab6284be22cd4793e9ba3d8b4c7297c3c934231d
ahvigil <ahvigil@mail.sfsu.edu>
2014-07-27 02:14:28 -0700

    spotify route

--- oslplans/app.js
+++ oslplans/app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var spotifyauth = require('./routes/spotifyauth');
+var spotify = require('./routes/spotify');
 var home = require('./routes/home');
 
 var app = express();
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/spotifyauth', spotifyauth);
+app.use('/spotify', spotify);
 app.use('/home', home);
 
 /// catch 404 and forward to error handler
