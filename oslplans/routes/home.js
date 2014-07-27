0c5f5dfa14691799e5e8612fb47bf6cf76573089
ahvigil <ahvigil@mail.sfsu.edu>
2014-07-27 01:58:47 -0700

    JSON.parse

--- oslplans/routes/home.js
+++ oslplans/routes/home.js
@@ -14,7 +14,7 @@ router.get('/', function(req, res) {
   };
   
   request.get(options, function(error, response, body) {
-    user_id = JSON.parse(body).id
+    user_id = JSON.parse(body).id;
     console.log(user_id);
   });
   
