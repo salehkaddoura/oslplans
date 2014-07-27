c9402af1e174676e00fbc21ff62937e7389017c8
Ubuntu <ubuntu@ip-172-31-22-16.us-west-2.compute.internal>
2014-07-27 01:02:49 +0000

    edit index

--- oslplans/app.js
+++ oslplans/app.js
@@ -13,7 +13,6 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.set('title', 'Outside Plans');
 
 app.use(favicon());
 app.use(logger('dev'));
