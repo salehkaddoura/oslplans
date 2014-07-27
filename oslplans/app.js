0eb0587aee991dd5c7c8002a3bc1c77a6828fd69
Saleh Kaddoura <saleh.kaddoura@gmail.com>
2014-07-26 21:22:48 -0700

    fixing server paths in app.js

--- oslplans/app.js
+++ oslplans/app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var login = require('./routes/login.js')
+var login = require('./routes/login.js');
 var 
 
 var app = express();
