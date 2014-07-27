369a76d1d61e56f783757a034d2588b80939e7f8
ahvigil <ahvigil@mail.sfsu.edu>
2014-07-26 17:53:46 -0700

    rename application

--- oslplans/app.js
+++ oslplans/app.js
@@ -13,6 +13,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('title', 'Outside Plans');
 
 app.use(favicon());
 app.use(logger('dev'));
