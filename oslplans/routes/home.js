16038519f4be0786f35ba58bd32c4150574557b2
ahvigil <ahvigil@mail.sfsu.edu>
2014-07-27 02:25:34 -0700

    remove home api calls

--- oslplans/routes/home.js
+++ oslplans/routes/home.js
@@ -1,24 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var request = require('request'); // "Request" library
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  access_token = req.query.code;
-  console.log("Recieved access token: " + access_token);
-  
-  var options = {
-    url: 'https://api.spotify.com/v1/me',
-    headers: { 'Authorization': 'Bearer ' + access_token },
-    json: true
-  };
-  
-  request.get(options, function(error, response, body) {
-    console.log(body);
-  });
-  
-  
-  
   res.render('home', { title: 'Express' });
 });
 
