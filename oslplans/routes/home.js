6360dbadf3ccd6f887f474a601b38a3e92e11646
ahvigil <ahvigil@mail.sfsu.edu>
2014-07-27 02:29:29 -0700

    request library

--- oslplans/routes/home.js
+++ oslplans/routes/home.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request'); // "Request" library
 
 /* GET home page. */
 router.get('/', function(req, res) {
